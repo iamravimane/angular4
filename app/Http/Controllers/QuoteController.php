b7e2e55fd5b19f1342c45dc6709655ba9423922c
unknown <RMane@ADT046.allied.com>
2018-04-05 21:00:41 +0530

    start angular4 project

---
+++ app/Http/Controllers/QuoteController.php
@@ -0,0 +1,54 @@
+<?php namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Quote;
+
+class QuoteController extends Controller
+{
+
+
+   public function postQuote(Request $request)
+   {
+
+   	$quote=new  Quote();
+   	$quote->content=$request->input('content');
+   	$quote->save();
+   	return response()->json(['quote'=>$quote],201);
+	}
+
+   public function getQuotes()
+   {
+   	
+   	$quotes=Quote::all();
+
+   	$response=['quotes'=>$quotes];
+
+   	return response()->json($response,200);
+   }
+
+   public function putQuote(Request $request,$id)
+   {
+   		$quote=Quote::find($id);
+
+   		if(!$quote){
+   			return response()->json(['message'=>'Document not found']);
+
+   			$quote->content=$request->input('content');
+   			$quote->save();
+   			 	return response()->json(['quote'=>$quote],200);
+
+
+   		}
+
+   }
+
+   public function deleteQuote($id)
+   {
+
+   	$quote=Quote::find($id);
+   	$quote->delete();
+
+   	return response()->json(['message'=>'Quote deleted'],200);
+   	
+   }
+}
